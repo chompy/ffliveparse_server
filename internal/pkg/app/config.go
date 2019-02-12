2e920ed85148504c8fee4d5186a81be3c1c98bf8
Nathan Ogden <nathan@ogden.tech>
2019-02-11 22:47:54 -0500

    version bump

--- internal/pkg/app/config.go
+++ internal/pkg/app/config.go
@@ -20,7 +20,7 @@ package app
 import "fmt"
 
 // VersionNumber - version number
-const VersionNumber int32 = 111
+const VersionNumber int32 = 112
 
 // ActPluginMinVersionNumber - act plugin min version
 const ActPluginMinVersionNumber int32 = 5
