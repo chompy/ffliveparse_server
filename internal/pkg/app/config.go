eb2d3a08262f6f60fbb943e21711f56b2a43cc5b
Nathan Ogden <nathan@ogden.tech>
2019-06-27 22:54:40 -0400

    version bump; added stat page graph for connections; added robots.txt

--- internal/pkg/app/config.go
+++ internal/pkg/app/config.go
@@ -20,7 +20,7 @@ package app
 import "fmt"
 
 // VersionNumber - version number
-const VersionNumber int32 = 120
+const VersionNumber int32 = 121
 
 // ActPluginMinVersionNumber - act plugin min version
 const ActPluginMinVersionNumber int32 = 5
